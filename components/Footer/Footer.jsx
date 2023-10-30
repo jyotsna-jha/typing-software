c040c73af143e657b9acd43e278971114cf65c48
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2023-10-30 19:54:34 +0545

    footer commit2

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,29 +5,36 @@ import { Icons } from "./Menu";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#2980b9] text-white">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#e74c3c] py-7">
+    <footer className="bg-gray-900 text-white">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
         <h1
-          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5"
+          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
         >
-          <span className="text-[#6ab04c]">Type </span>like a pro, at your own pace
+          <span className="text-teal-400">Type</span> like a pro, at your own pace
+
         </h1>
         <div>
           <input
             type="text"
             placeholder="Enter Your Email"
-            className="text-[#2d98da] sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
+            className="text-gray-800
+           sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
           />
           <button
-            className="bg-[#6ab04c] hover:bg-[#e74c3c] duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full"
+            className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
+           rounded-md text-white md:w-auto w-full"
           >
             Reach Us
           </button>
         </div>
       </div>
       <ItemsContainer />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-[#dcdde1] text-sm pb-8">
-        <span>© 2023 Anand Typing. All rights reserved.</span>
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
+      text-center pt-2 text-gray-400 text-sm pb-8"
+      >
+        <span>© 2020 Appy. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
         <SocialIcons Icons={Icons} />
       </div>
