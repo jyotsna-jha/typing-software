b5f53c3915c578423ad5d84c2954269162c34bfb
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2023-10-31 12:27:17 +0545

    Herosection colorchange

--- components/Hero.jsx/Hero1.jsx
+++ components/Hero.jsx/Hero1.jsx
@@ -11,11 +11,11 @@ import {
 const HomePage = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-screen-xl p-4 md:p-8 bg-[#2c3e50] shadow-lg rounded-lg text-center text-[#dcdde1]">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-[#dcdde1] mb-4 font-poppins">
+      <div className="w-full max-w-screen-xl p-4 md:p-8 bg-gray-900 shadow-lg rounded-lg text-center text-[#e74c3c]">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-extrabold text-[#e74c3c] mb-4 font-poppins">
           Typing Practice for All
         </h1>
-        <p className="text-sm md:text-base lg:text-lg text-[#dcdde1] mb-8 font-poppins">
+        <p className="text-sm md:text-base lg:text-lg text-[#7f8c8d] mb-8 font-poppins">
           Improve your typing skills, speed, and accuracy in multiple languages with our tests. Test your Hindi, English, or Unicode typing abilities. Explore translation services, MCQ tests, and valuable resources.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -30,14 +30,14 @@ const HomePage = () => {
             <div
               key={index}
               className="group relative transform transition-transform hover:scale-105 hover:shadow-md">
-              <div className="bg-[#e74c3c] p-6 rounded-lg text-center transition-transform transform hover:scale-105 hover:bg-opacity-100">
+              <div className="bg-[#ffffff19] p-6 rounded-lg text-center transition-transform transform hover:scale-105 hover:bg-[#e74c3c] hover:bg-opacity-100">
                 {button.icon}
-                <p className="font-semibold text-base lg:text-lg text-[#dcdde1] font-poppins">
+                <p className="font-semibold text-base lg:text-lg text-[#bdc3c7] font-poppins">
                   {button.text}
                 </p>
               </div>
               <div className="hidden group-hover:block absolute top-0 left-0 w-full h-full bg-opacity-75 bg-white transition-opacity opacity-0 hover:opacity-100">
-                <p className="h-full flex items-center justify-center text-sm md:text-base lg:text-lg text-[#2c3e50] font-poppins">
+                <p className="h-full flex items-center justify-center text-sm md:text-base lg:text-lg text-gray-900 font-poppins">
                   {button.text}
                 </p>
               </div>
