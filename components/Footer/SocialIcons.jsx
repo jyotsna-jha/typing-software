a0eca8043d0a1f411d40874c322aab11ddd114cb
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2024-02-06 22:43:02 +0545

    social media links

--- components/Footer/SocialIcons.jsx
+++ components/Footer/SocialIcons.jsx
@@ -10,15 +10,18 @@ const SocialIcons = ({ Icons }) => {
 
   return (
     <div className="text-[#2d98da]"> {/* Use the same color as your footer */}
-      <FaInstagram
-        style={iconStyle}
-        className="text-[#d62976] p-2 cursor-pointer inline-flex items-center rounded-full bg-gray-700 mx-1.5 hover:text-[#e74c3c] hover-bg-[#6ab04c] duration-300"
-      />
-       <FaYoutube
-        style={iconStyle}
-        className="text-[#e04949] p-2 cursor-pointer inline-flex items-center rounded-full bg-gray-700 mx-1.5 hover:text-[#e74c3c] hover-bg-[#6ab04c] duration-300"
-      />
-    
+      <a href="https://www.instagram.com/anand.institute?igsh=MW9kYnZhaXJmNTEyeg%3D%3D" target="_blank" rel="noopener noreferrer">
+        <FaInstagram
+          style={iconStyle}
+          className="text-[#d62976] p-2 cursor-pointer inline-flex items-center rounded-full bg-gray-700 mx-1.5 hover:text-[#e74c3c] hover-bg-[#6ab04c] duration-300"
+        />
+      </a>
+      <a href="https://www.youtube.com/@AnandCompetitiveComputerClass" target="_blank" rel="noopener noreferrer">
+        <FaYoutube
+          style={iconStyle}
+          className="text-[#e04949] p-2 cursor-pointer inline-flex items-center rounded-full bg-gray-700 mx-1.5 hover:text-[#e74c3c] hover-bg-[#6ab04c] duration-300"
+        />
+      </a>
     </div>
   );
 };
